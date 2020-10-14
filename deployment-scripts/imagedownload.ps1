6bd8517add37b64770ec89762004d1b948ed7346
Martin Nygaard Ehrnst / Intility AS <Martin.Ehrnst@intility.no>
2020-10-14 10:01:19 +0200

    ,,

--- deployment-scripts/imagedownload.ps1
+++ deployment-scripts/imagedownload.ps1
@@ -1,9 +1,18 @@
 # download a few images from google images and add them to a our storage account
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $searchString
+)
+
 
 $downloadFolder = $env:TEMP
-$googleImageSearch = (invoke-webrequest -Uri "https://www.google.com/search?q=marcel+zehner&safe=active&source=lnms&tbm=isch").images `
+Write-Verbose "searching for images of $searchString"
+$googleImageSearch = (invoke-webrequest -Uri "https://www.google.com/search?q=$searchString&safe=active&source=lnms&tbm=isch").images `
 | Where-Object {$_.src -like "https://encrypted*"} | Select-Object src -First 10
 foreach ($result in $googleImageSearch) {
+    
     $guid = (new-guid).Guid
     $imgName = "${downloadFolder}\$guid.png"
     Invoke-WebRequest -Uri $result.src -OutFile $imgName -UseBasicParsing
